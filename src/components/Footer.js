483da2c469358355cfb5f8d2cf322315ba7bf5ce
Anjali Mehta <121865600+anjalimehta0310@users.noreply.github.com>
2024-02-22 21:23:21 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 function Footer() {
-    return (
-        <div className="text-center p-6 bg-black footer">
-            <span className="text-white">&copy; 2023 Copyright - </span>
-            <a className="link-underline text-orange-500 font-bold">Anjali Mehta</a>
-        </div>
-    );
+  return (
+    <div className="text-center p-6 bg-black footer">
+      <span className="text-white">&copy; 2024 Copyright - </span>
+      <button className="link-underline text-orange-600 font-bold">
+        Anjali Mehta
+      </button>
+    </div>
+  );
 }
 
 export default Footer;
