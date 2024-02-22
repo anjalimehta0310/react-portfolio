b6fe78202420eeaa66fc3cfff8f0489b38af2d2f
Anjali Mehta <121865600+anjalimehta0310@users.noreply.github.com>
2024-02-22 21:23:48 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -31,8 +31,11 @@ const NavBar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed nav">
       <div>
-        {/* <h1 className="text-5xl font-signature ml-2"><a className="link-underline hover:transition ease-in-out delay-150 hover:underline hover:decoration-solid" href="https://rahulkarda.netlify.app">Rahul</a></h1> */}
-        <h1 className="text-5xl font-signature ml-2"><a href="#"className="link-underline link-underline-black"  target="_blank" rel="noreferrer">Anjali</a></h1>
+        <h1 className="text-5xl font-signature ml-2">
+          <button className="link-underline link-underline-black" onClick={() => window.scrollTo(0, 0)}>
+            Anjali
+          </button>
+        </h1>
       </div>
 
       <ul className="hidden md:flex">
