7db27bc70cb19cf646ab8bc9630aa8b6620dbb74
Anjali Mehta <121865600+anjalimehta0310@users.noreply.github.com>
2024-04-24 14:50:27 +0530

    Update Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,7 +3,9 @@ import chatbot from "../assets/utlits/chatbot.webp";
 import voiceAssistant from "../assets/utlits/voiceAssistant.png"
 import musicPlayer from "../assets/utlits/music player.png";
 import food from "../assets/utlits/food.png";
-
+import netflixgpt from "../assets/utlits/netflixGpt.png";
+import youtubeClone from "../assets/utlits/youtubeClone.png";
+import todolist from "../assets/utlits/ToDoList.jpg"
 
 const Portfolio = () => {
   const portfolios = [
@@ -25,17 +27,32 @@ const Portfolio = () => {
     {
       id: 4,
       src: food,
-      link:"https://courageous-froyo-7d3119.netlify.app/",
+      link: "https://courageous-froyo-7d3119.netlify.app/",
       repo: 'https://github.com/anjalimehta0310/FoodWebsite'
     },
-   
+    {
+      id: 5,
+      src: netflixgpt,
+      repo: "https://github.com/anjalimehta0310/netflix-gpt"
+    },
+    {
+      id: 6,
+      src: youtubeClone,
+      repo: "https://github.com/anjalimehta0310/youtubeClone"
+    },
+    {
+      id: 7,
+      src: todolist,
+      repo: "https://github.com/anjalimehta0310/to-do-list"
+    },
     
   ];
 
   return (
     <div
       name="portfolio"
-      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen portfolio"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white min-h-screen portfolio"
+      style={{ minHeight: "calc(100vh - 80px)" }} 
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
@@ -46,7 +63,7 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src,link, repo }) => (
+          {portfolios.map(({ id, src, link, repo }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
@@ -54,10 +71,12 @@ const Portfolio = () => {
                 className="rounded-md duration-200 hover:scale-105"
               />
               <div className="flex items-center justify-center">
-              <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105" onClick={ () => window.open(link, '_blank')}>
-                  Demo
-                </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105" onClick={ () => window.open(repo, '_blank')}>
+                {link && (
+                  <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105" onClick={() => window.open(link, '_blank')}>
+                    Demo
+                  </button>
+                )}
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105" onClick={() => window.open(repo, '_blank')}>
                   GitHub
                 </button>
               </div>
